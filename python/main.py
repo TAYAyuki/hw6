5fb2704e1267c02660b9f79c5138d019c3ae610c
田屋　侑希 <yuki@Yuki-3.local>
2019-07-05 12:08:02 +0900

    @0error

--- python/main.py
+++ python/main.py
@@ -8,38 +8,52 @@ import random
 import webapp2
 
 # Reads json description of the board and provides simple interface.
+#黒:1　白:2
+#{"board":
+#{"Pieces":[[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],
+#[0,0,0,2,1,0,0,0],[0,0,0,1,1,0,0,0],[0,0,0,0,1,0,0,0],
+# [0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0]],"Next":2},
+# "gamekey":"ag5zfnN0ZXAtcmV2ZXJzaXIRCxIER2FtZRiAgKCjzfKMCgw",
+# "history":[{"Where":[5,6],"As":1}],
+# "valid_moves":[{"Where":[6,4],"As":2},{"Where":[4,6],"As":2},{"Where":[6,6],"As":2}]}
+
+#game
+#{ "board":{"Pieces":[],"Next":0or2}, "gamekey":"", "history":[{"Where":[],"As":1or2}], "valid_moves":[{},{}.. }
+
+#g = Game(self.request.get('json'))
 class Game:
 	# Takes json or a board directly.
 	def __init__(self, body=None, board=None):
-                if body:
-		        game = json.loads(body)
-                        self._board = game["board"]
-                else:
-                        self._board = board
+		if body:
+			game = json.loads(body)
+			self._board = game["board"]
+			#{"Pieces":[配列],"Next":1 or 2}
+		else:
+			self._board = board
 
 	# Returns piece on the board.
 	# 0 for no pieces, 1 for player 1, 2 for player 2.
 	# None for coordinate out of scope.
 	def Pos(self, x, y):
-		return Pos(self._board["Pieces"], x, y)
+		return Pos(self._board["Pieces"], x, y) #Pos(オセロの配列,x,y)
 
 	# Returns who plays next.
 	def Next(self):
 		return self._board["Next"]
-
+	#1 of 2
 	# Returns the array of valid moves for next player.
 	# Each move is a dict
 	#   "Where": [x,y]
 	#   "As": player number
+
 	def ValidMoves(self):
-                moves = []
-                for y in xrange(1,9):
-                        for x in xrange(1,9):
-                                move = {"Where": [x,y],
-                                        "As": self.Next()}
-                                if self.NextBoardPosition(move):
-                                        moves.append(move)
-                return moves
+		moves = []
+		for y in xrange(1,9):
+			for x in xrange(1,9):
+				move = {"Where": [x,y],"As": self.Next()}
+				if self.NextBoardPosition(move):
+					moves.append(move)
+		return moves
 
 	# Helper function of NextBoardPosition.  It looks towards
 	# (delta_x, delta_y) direction for one of our own pieces and
@@ -56,64 +70,63 @@ class Game:
 			look_x += delta_x
 			look_y += delta_y
 		if Pos(new_board, look_x, look_y) == player and len(flip_list) > 0:
-                        # there's a continuous line of our opponents
-                        # pieces between our own pieces at
-                        # [look_x,look_y] and the newly placed one at
-                        # [x,y], making it a legal move.
+            # there's a continuous line of our opponents
+            # pieces between our own pieces at
+	        # [look_x,look_y] and the newly placed one at
+            # [x,y], making it a legal move.
 			SetPos(new_board, x, y, player)
 			for flip_move in flip_list:
 				flip_x = flip_move[0]
 				flip_y = flip_move[1]
 				SetPos(new_board, flip_x, flip_y, player)
-                        return True
-                return False
+			return True
+		return False
 
 	# Takes a move dict and return the new Game state after that move.
 	# Returns None if the move itself is invalid.
+
 	def NextBoardPosition(self, move):
 		x = move["Where"][0]
 		y = move["Where"][1]
-                if self.Pos(x, y) != 0:
-                        # x,y is already occupied.
-                        return None
+		if self.Pos(x, y) != 0:
+			# x,y is already occupied.
+			return None
 		new_board = copy.deepcopy(self._board)
-                pieces = new_board["Pieces"]
-
+		pieces = new_board["Pieces"]
 		if not (self.__UpdateBoardDirection(pieces, x, y, 1, 0)
-                        | self.__UpdateBoardDirection(pieces, x, y, 0, 1)
-		        | self.__UpdateBoardDirection(pieces, x, y, -1, 0)
-		        | self.__UpdateBoardDirection(pieces, x, y, 0, -1)
-		        | self.__UpdateBoardDirection(pieces, x, y, 1, 1)
-		        | self.__UpdateBoardDirection(pieces, x, y, -1, 1)
-		        | self.__UpdateBoardDirection(pieces, x, y, 1, -1)
-		        | self.__UpdateBoardDirection(pieces, x, y, -1, -1)):
-                        # Nothing was captured. Move is invalid.
-                        return None
-
-                # Something was captured. Move is valid.
-                new_board["Next"] = 3 - self.Next()
+				| self.__UpdateBoardDirection(pieces, x, y, 0, 1)
+				| self.__UpdateBoardDirection(pieces, x, y, -1, 0)
+				| self.__UpdateBoardDirection(pieces, x, y, 0, -1)
+				| self.__UpdateBoardDirection(pieces, x, y, 1, 1)
+				| self.__UpdateBoardDirection(pieces, x, y, -1, 1)
+				| self.__UpdateBoardDirection(pieces, x, y, 1, -1)
+				| self.__UpdateBoardDirection(pieces, x, y, -1, -1)):
+			# Nothing was captured. Move is invalid.
+			return None
+		# Something was captured. Move is valid.
+		new_board["Next"] = 3 - self.Next()
 		return Game(board=new_board)
 
-	def CountBlacek_White(self,g,color):
+	def CountBlack_White(self,color):
 		counter = 0
-		for piece in g._board["Pieces"]:
+		for piece in self._board["Pieces"]:
 			counter += piece.count(color)
 		return counter
 
 	def WorstScore(self,player):
-	    if player == 1:
-	        return 70
-	    else:
-	        return -70
+		if player == 1:
+			return 70
+		else:
+			return -70
 
 
-	def MinMaxScore(self, depth, g):
+	def MinMaxScore(self, depth,g):
 		best_move = {"Where":[0,0],"As":1}
 		valid_moves = self.ValidMoves()
 		player = valid_moves[0]["As"]
 		best = self.WorstScore(player)
 		if depth < 1:
-			return self.CountBlacek_White(g,1) - self.CountBlacek_White(g,2)
+			return self.CountBlack_White(1) - self.CountBlack_White(2)
 		for move in valid_moves:
 			nextBoard = self.NextBoardPosition(move)
 			score = self.MinMaxScore(depth-1,nextBoard)
@@ -159,6 +172,8 @@ def PrettyMove(move):
 	return '%s%d' % (chr(ord('A') + m[0] - 1), m[1])
 
 
+
+#webapp2.WSGIApplicationで呼び出される
 class MainHandler(webapp2.RequestHandler):
     # Handling GET request, just for debugging purposes.
     # If you open this handler directly, it will show you the
@@ -175,7 +190,7 @@ Paste JSON here:<p/><textarea name=json cols=80 rows=24></textarea>
 """)
           return
         else:
-          g = Game(self.request.get('json'))
+          g = Game(self.request.get('json')) #はじめに呼び出される g = {"Pieces":[配列],"Next":1 or 2}
           self.pickMove(g)
 
     def post(self):
@@ -186,18 +201,19 @@ Paste JSON here:<p/><textarea name=json cols=80 rows=24></textarea>
 
 
     def pickMove(self, g):
-    	# Gets all valid moves.
-    	valid_moves = g.ValidMoves()
-    	if len(valid_moves) == 0:
-    		# Passes if no valid moves.
-    		self.response.write("PASS")
-    	else:
+		# Gets all valid moves.
+		valid_moves = g.ValidMoves()
+		#置くことができるますのリスト
+		if len(valid_moves) == 0:
+			# Passes if no valid moves.
+			self.response.write("PASS")
+		else:
     		# Chooses a valid move randomly if available.
-			# TO STEP STUDENTS:
-			# You'll probably want to change how this works, to do something
-			# more clever than just picking a random move.
+            # TO STEP STUDENTS:
+            # You'll probably want to change how this works, to do something
+            # more clever than just picking a random move.
 			#move = random.choice(valid_moves)
-			best_move = g.MinMaxScore(3, g)
+			best_move = g.MinMaxScore(3,g)
 			self.response.write(PrettyMove(best_move))
 
 app = webapp2.WSGIApplication([
